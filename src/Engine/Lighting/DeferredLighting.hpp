d1cdd2ae029a22074eb209c73c94e05012a26968
Albin Bernhardsson <catscratchesp@gmail.com>
2017-08-31 19:17:29 +0200

    Remove unused framebuffer

--- src/Engine/Lighting/DeferredLighting.hpp
+++ src/Engine/Lighting/DeferredLighting.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <GL/glew.h>
-
 class World;
 class Entity;
 namespace Video {
@@ -11,26 +9,11 @@ namespace Video {
 /// Holds the frame buffers used for deferred rendering.
 class DeferredLighting {
     public:
-        /// The different buffer types.
-        enum TEXTURE_TYPE {
-            DIFFUSE, ///< Diffuse texture
-            NORMAL, ///< Normals
-            SPECULAR, ///< Specular
-            GLOW, ///< Glow
-            NUM_TEXTURES ///< Total number of textures (excluding depth buffer)
-        };
-        
         /// Create new render target.
         /**
          * @param renderer Renderer to use for rendering.
          */
         DeferredLighting(Video::Renderer* renderer);
-        
-        /// Destructor
-        /**
-         * Free allocated resources.
-         */
-        ~DeferredLighting();
         
         /// Render the lighting in the world.
         /**
@@ -40,23 +23,5 @@ class DeferredLighting {
         void Render(World& world, const Entity* camera);
         
     private:
-        static void AttachTexture(GLuint texture, unsigned int width, unsigned int height, GLenum attachment, GLint internalFormat);
-        
         Video::Renderer* renderer;
-        
-        GLuint textures[NUM_TEXTURES];
-        
-        GLuint frameBufferObject;
-        GLuint depthHandle;
-        
-        // Store light uniform locations so we don't have to get them every frame.
-        static const unsigned int lightCount = 32U;
-        struct {
-            GLint position;
-            GLint intensities;
-            GLint attenuation;
-            GLint ambientCoefficient;
-            GLint coneAngle;
-            GLint direction;
-        } lightUniforms[lightCount];
 };
