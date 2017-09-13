0442771b41032f3f427e0a4fda6de7168037951e
Andreas Flöjt <andenzz@hotmail.com>
2017-09-13 08:55:38 +0200

    Basic physics components.

---
+++ src/Physics/RigidBody.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class btRigidBody;
+
+namespace Physics {
+
+    class RigidBody {
+        friend class Simulator;
+
+        public:
+            /// Constructor.
+            RigidBody();
+
+            /// Destructor.
+            ~RigidBody();
+
+            /// Set the mass of a rigid body.
+            /**
+             * @param mass Mass in kilograms.
+             */
+            void Mass(float mass);
+
+            /// Get the position of a rigid body.
+            /**
+             * @return Position of the rigid body.
+             */
+            glm::vec3 Position();
+
+            void Position(glm::vec3 const& pos);
+
+        private:
+            RigidBody(RigidBody& other) = delete;
+            void operator=(RigidBody& rhs) = delete;
+
+        private:
+            btRigidBody* rigidBody = nullptr;
+            Simulator* simulator = nullptr;
+    };
+
+}
