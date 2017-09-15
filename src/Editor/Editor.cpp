acb0d06cd825e07ab8f38146e4b12ca6eb65abac
DESKTOP-CQL9F7S\PickleRick <aston_martin93@hotmail.com>
2017-09-15 18:12:24 +0200

    Added focus on default location (0,0,0). Added 'F'-key to input.

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -20,15 +20,16 @@
 Editor::Editor() {
     // Create Hymns directory.
     FileSystem::CreateDirectory((FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns").c_str());
-    
+
     // Load theme.
     std::string theme = EditorSettings::GetInstance().GetString("Theme");
     if (FileSystem::FileExists((FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Themes" + FileSystem::DELIMITER + theme + ".json").c_str())) {
         ImGui::LoadTheme(theme.c_str());
-    } else {
+    }
+    else {
         ImGui::LoadDefaultTheme();
     }
-    
+
     // Assign controls.
     Input()->AssignButton(InputHandler::PROFILE, InputHandler::KEYBOARD, GLFW_KEY_F2);
     Input()->AssignButton(InputHandler::PLAYTEST, InputHandler::KEYBOARD, GLFW_KEY_F5);
@@ -42,12 +43,13 @@ Editor::Editor() {
     Input()->AssignButton(InputHandler::LEFT, InputHandler::KEYBOARD, GLFW_KEY_A);
     Input()->AssignButton(InputHandler::RIGHT, InputHandler::KEYBOARD, GLFW_KEY_D);
     Input()->AssignButton(InputHandler::ZOOM, InputHandler::KEYBOARD, GLFW_KEY_Z);
-    
+    Input()->AssignButton(InputHandler::FOCUS, InputHandler::KEYBOARD, GLFW_KEY_F);
+
     // Create editor camera.
     cameraEntity = cameraWorld.CreateEntity("Editor Camera");
     cameraEntity->AddComponent<Component::Lens>();
     cameraEntity->position.z = 10.0f;
-    
+
     // Create cursors.
     cursors[0] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
     cursors[1] = glfwCreateStandardCursor(GLFW_IBEAM_CURSOR);
@@ -62,7 +64,7 @@ Editor::Editor() {
 
 Editor::~Editor() {
     // Destroy cursors.
-    for (int i=0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i) {
         glfwDestroyCursor(cursors[i]);
     }
 }
@@ -76,8 +78,7 @@ void Editor::Show(float deltaTime) {
             savePromtWindow.SetVisible(true);
             savePromtWindow.Show();
 
-            switch (savePromtWindow.GetDecision())
-            {
+            switch (savePromtWindow.GetDecision()) {
             case 0:
                 Save();
                 savePromptAnswered = true;
@@ -216,41 +217,60 @@ void Editor::Show(float deltaTime) {
                 lastX = Input()->GetCursorX();
                 lastY = Input()->GetCursorY();
             }
-        
+
             float sensitivity = 0.3f;
             cameraEntity->rotation.x += sensitivity * (Input()->GetCursorX() - lastX);
             cameraEntity->rotation.y += sensitivity * (Input()->GetCursorY() - lastY);
-        
+
             lastX = Input()->GetCursorX();
             lastY = Input()->GetCursorY();
-        
+
             glm::mat4 orientation = cameraEntity->GetCameraOrientation();
             glm::vec3 backward(orientation[0][2], orientation[1][2], orientation[2][2]);
             glm::vec3 right(orientation[0][0], orientation[1][0], orientation[2][0]);
-        
+
             //Move speed scaling.
             float speed = 10.0f * deltaTime * (glm::abs(cameraEntity->position.y) / 10.0f);
             float constantSpeed = 10.0f * deltaTime;
-        
+
             if (cameraEntity->position.y > 10.0f || cameraEntity->position.y < -10.0f) {
                 cameraEntity->position += speed * backward * static_cast<float>(Input()->Pressed(InputHandler::BACKWARD) - Input()->Pressed(InputHandler::FORWARD));
                 cameraEntity->position += speed * right * static_cast<float>(Input()->Pressed(InputHandler::RIGHT) - Input()->Pressed(InputHandler::LEFT));
-            } else { 
+            }
+            else {
                 cameraEntity->position += constantSpeed * backward * static_cast<float>(Input()->Pressed(InputHandler::BACKWARD) - Input()->Pressed(InputHandler::FORWARD));
                 cameraEntity->position += constantSpeed * right * static_cast<float>(Input()->Pressed(InputHandler::RIGHT) - Input()->Pressed(InputHandler::LEFT));
             }
+        }
 
+        // Move camera position and rotation to fixate on selected object
+        if (Input()->Triggered(InputHandler::FOCUS)) {
 
+            glm::vec3 backward = glm::normalize(cameraEntity->position - glm::vec3(0, 0, 0)); // When mousepicking is added, change this vec3 to object pivot
 
+            while (glm::length(glm::vec3(0, 0, 0) - cameraEntity->position) > 10) {
+                cameraEntity->position -= backward;
+            }
+
+            while (glm::length(glm::vec3(0, 0, 0) - cameraEntity->position) < 10) {
+                cameraEntity->position += backward;
+            }
+
+            glm::vec3 camDirection = glm::vec3(0, 0, 0) - cameraEntity->position; // When mousepicking is added, change this vec3 to object pivot. NOTE Does this do the same as 'backward' ?
+            glm::normalize(camDirection);
+
+            float yaw = std::atan2(camDirection.x, -camDirection.z);
+            cameraEntity->rotation.x = glm::degrees(yaw);
+
+            float xz = std::sqrtf(camDirection.x * camDirection.x + camDirection.z * camDirection.z);
+            float pitch = std::atan2(-camDirection.y, xz);
+            cameraEntity->rotation.y = glm::degrees(pitch);
 
         }
 
-        glm::vec3 test = glm::vec3();
-
         //Scroll zoom.
         if (Input()->GetScrollDown()) {
-            if (!ImGui::IsMouseHoveringAnyWindow())
-            {
+            if (!ImGui::IsMouseHoveringAnyWindow()) {
                 glm::mat4 orientation = cameraEntity->GetCameraOrientation();
                 glm::vec3 backward(orientation[0][2], orientation[1][2], orientation[2][2]);
                 float speed = 10.0f * deltaTime * (glm::length(cameraEntity->position) / 10.0f);
@@ -258,15 +278,14 @@ void Editor::Show(float deltaTime) {
             }
         }
         if (Input()->GetScrollUp()) {
-            if (!ImGui::IsMouseHoveringAnyWindow())
-            {
+            if (!ImGui::IsMouseHoveringAnyWindow()) {
                 glm::mat4 orientation = cameraEntity->GetCameraOrientation();
                 glm::vec3 backward(orientation[0][2], orientation[1][2], orientation[2][2]);
                 float speed = 10.0f * deltaTime * (glm::length(cameraEntity->position) / 10.0f);
                 cameraEntity->position += speed * backward * -10.0f;
             }
         }
-    
+
 
         if (Input()->Triggered(InputHandler::PLAYTEST) && Hymn().GetPath() != "")
             play = true;
@@ -344,21 +363,21 @@ void Editor::NewHymnClosed(const std::string& hymn) {
         resourceList.ResetScene();
         Hymn().Clear();
         Hymn().world.CreateRoot();
-        Hymn().SetPath(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER +  hymn);
+        Hymn().SetPath(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER + hymn);
         resourceList.SetVisible(true);
-        
+
         // Default scene.
         Hymn().scenes.push_back("Scene #0");
-        
+
         Entity* player = Hymn().world.GetRoot()->AddChild("Player");
         player->position.z = 10.f;
         player->AddComponent<Component::Lens>();
         player->AddComponent<Component::Listener>();
-        
+
         Entity* sun = Hymn().world.GetRoot()->AddChild("Sun");
         sun->AddComponent<Component::DirectionalLight>();
     }
-    
+
     selectHymnWindow.SetVisible(false);
 }
 
@@ -374,9 +393,9 @@ void Editor::OpenHymnClosed(const std::string& hymn) {
     // Open hymn.
     if (!hymn.empty()) {
         resourceList.ResetScene();
-        Hymn().Load(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER +  hymn);
+        Hymn().Load(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER + hymn);
         resourceList.SetVisible(true);
     }
-    
+
     selectHymnWindow.SetVisible(false);
 }
