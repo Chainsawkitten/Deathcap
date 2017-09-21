ea88d4a477d8d805cc1da0195d26c0f9c922fa84
Albin Bernhardsson <catscratchesp@gmail.com>
2017-09-21 09:20:46 +0200

    Save, load and clear resources

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -13,6 +13,7 @@
 #include <Engine/Component/Lens.hpp>
 #include <Engine/Component/Listener.hpp>
 #include "ImGui/Theme.hpp"
+#include "Resources.hpp"
 
 #include <imgui.h>
 #include <GLFW/glfw3.h>
@@ -20,14 +21,14 @@
 Editor::Editor() {
     // Create Hymns directory.
     FileSystem::CreateDirectory((FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns").c_str());
-
+    
     // Load theme.
     std::string theme = EditorSettings::GetInstance().GetString("Theme");
     if (FileSystem::FileExists((FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Themes" + FileSystem::DELIMITER + theme + ".json").c_str()))
         ImGui::LoadTheme(theme.c_str());
     else
         ImGui::LoadDefaultTheme();
-
+    
     // Assign controls.
     Input()->AssignButton(InputHandler::PROFILE, InputHandler::KEYBOARD, GLFW_KEY_F2);
     Input()->AssignButton(InputHandler::PLAYTEST, InputHandler::KEYBOARD, GLFW_KEY_F5);
@@ -42,22 +43,21 @@ Editor::Editor() {
     Input()->AssignButton(InputHandler::RIGHT, InputHandler::KEYBOARD, GLFW_KEY_D);
     Input()->AssignButton(InputHandler::ZOOM, InputHandler::KEYBOARD, GLFW_KEY_Z);
     Input()->AssignButton(InputHandler::FOCUS, InputHandler::KEYBOARD, GLFW_KEY_F);
-
+    
     // Create editor camera.
     cameraEntity = cameraWorld.CreateEntity("Editor Camera");
     cameraEntity->AddComponent<Component::Lens>();
     cameraEntity->position.z = 10.0f;
-
+    
     // Create cursors.
     cursors[0] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
     cursors[1] = glfwCreateStandardCursor(GLFW_IBEAM_CURSOR);
     cursors[2] = glfwCreateStandardCursor(GLFW_CROSSHAIR_CURSOR);
     cursors[3] = glfwCreateStandardCursor(GLFW_VRESIZE_CURSOR);
     cursors[4] = glfwCreateStandardCursor(GLFW_HRESIZE_CURSOR);
-
+    
     savePromptAnswered = false;
     close = false;
-
 }
 
 Editor::~Editor() {
@@ -68,14 +68,12 @@ Editor::~Editor() {
 }
 
 void Editor::Show(float deltaTime) {
-
     if (close) {
-
+        // Ask the user whether they wish to save.
         if (Hymn().GetPath() != "") {
-
             savePromtWindow.SetVisible(true);
             savePromtWindow.Show();
-
+            
             switch (savePromtWindow.GetDecision()) {
             case 0:
                 Save();
@@ -89,19 +87,14 @@ void Editor::Show(float deltaTime) {
             default:
                 break;
             }
-
         } else {
-
             savePromptAnswered = true;
         }
-
-
     } else {
-
         bool play = false;
-
+        
         ImVec2 size(MainWindow::GetInstance()->GetSize().x, MainWindow::GetInstance()->GetSize().y);
-
+        
         // Main menu bar.
         if (ImGui::BeginMainMenuBar()) {
 
@@ -109,25 +102,23 @@ void Editor::Show(float deltaTime) {
             if (ImGui::BeginMenu("File")) {
                 if (ImGui::MenuItem("New Hymn", "CTRL+N"))
                     NewHymn();
-
+                
                 if (ImGui::MenuItem("Open Hymn", "CTRL+O"))
                     OpenHymn();
-
+                
                 if (Hymn().GetPath() != "") {
-
                     if (ImGui::MenuItem("Save Hymn", "CTRL+S"))
                         Save();
-
                 }
-
+                
                 ImGui::Separator();
-
+                
                 if (ImGui::MenuItem("Settings"))
                     settingsWindow.SetVisible(true);
-
+                
                 ImGui::EndMenu();
             }
-
+            
             // View menu.
             if (ImGui::BeginMenu("View")) {
                 static bool soundSources = EditorSettings::GetInstance().GetBool("Sound Source Icons");
@@ -224,7 +215,7 @@ void Editor::Show(float deltaTime) {
             glm::vec3 backward(orientation[0][2], orientation[1][2], orientation[2][2]);
             glm::vec3 right(orientation[0][0], orientation[1][0], orientation[2][0]);
 
-            //Move speed scaling.
+            // Move speed scaling.
             float speed = 10.0f * deltaTime * (glm::abs(cameraEntity->position.y) / 10.0f);
             float constantSpeed = 10.0f * deltaTime;
 
@@ -262,7 +253,7 @@ void Editor::Show(float deltaTime) {
             cameraEntity->rotation.y = glm::degrees(pitch);
         }
 
-        //Scroll zoom.
+        // Scroll zoom.
         if (Input()->GetScrollDown()) {
             if (!ImGui::IsMouseHoveringAnyWindow()) {
                 glm::mat4 orientation = cameraEntity->GetCameraOrientation();
@@ -279,8 +270,7 @@ void Editor::Show(float deltaTime) {
                 cameraEntity->position += speed * backward * -10.0f;
             }
         }
-
-
+        
         if (Input()->Triggered(InputHandler::PLAYTEST) && Hymn().GetPath() != "")
             play = true;
 
@@ -308,6 +298,7 @@ void Editor::Show(float deltaTime) {
 void Editor::Save() const {
     resourceView.SaveScene();
     Hymn().Save();
+    Resources().Save();
 }
 
 bool Editor::ReadyToClose() const {
@@ -356,6 +347,7 @@ void Editor::NewHymnClosed(const std::string& hymn) {
     if (!hymn.empty()) {
         resourceView.ResetScene();
         Hymn().Clear();
+        Resources().Clear();
         Hymn().world.CreateRoot();
         Hymn().SetPath(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER + hymn);
         resourceView.SetVisible(true);
@@ -389,6 +381,7 @@ void Editor::OpenHymnClosed(const std::string& hymn) {
     if (!hymn.empty()) {
         resourceView.ResetScene();
         Hymn().Load(FileSystem::DataPath("Hymn to Beauty") + FileSystem::DELIMITER + "Hymns" + FileSystem::DELIMITER + hymn);
+        Resources().Load();
         resourceView.SetVisible(true);
     }
 
