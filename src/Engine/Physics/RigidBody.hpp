3a2276bd389a34ec729038a8d3b9992d2c31b409
Andreas Flöjt <andenzz@hotmail.com>
2017-09-25 11:12:16 +0200

    Add class that wraps a Bullet rigid body.

---
+++ src/Engine/Physics/RigidBody.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include "IRigidBody.hpp"
+
+class btRigidBody;
+
+namespace Physics {
+    class Shape;
+
+    class RigidBody : public IRigidBody {
+        public:
+            /// Constructor.
+            RigidBody(Physics::Shape* shape);
+
+            /// Destructor.
+            ~RigidBody();
+
+            /// Set the mass of a rigid body.
+            /**
+            * @param mass Mass in kilograms.
+            */
+            virtual void Mass(float mass) override;
+
+            /// Get the position of a rigid body.
+            /**
+            * @return Position of the rigid body.
+            */
+            virtual glm::vec3 Position() override;
+
+            /// Set the position of a rigid body.
+            /**
+            * @param pos The new position.
+            */
+            virtual void Position(glm::vec3 const& pos) override;
+
+            btRigidBody* GetRigidBody();
+
+        private:
+            RigidBody(RigidBody& other) = delete;
+            void operator=(RigidBody& rhs) = delete;
+
+        private:
+            btRigidBody* rigidBody = nullptr;
+            Physics::Shape* shape = nullptr;
+    };
+
+}
