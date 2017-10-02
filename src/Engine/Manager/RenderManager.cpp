8b0f8bbe125e5e4302ed0caf8f7826bd7281c8d0
Albin Bernhardsson <catscratchesp@gmail.com>
2017-10-02 14:30:11 +0200

    Fix MSVC build

--- src/Engine/Manager/RenderManager.cpp
+++ src/Engine/Manager/RenderManager.cpp
@@ -204,10 +204,10 @@ void RenderManager::RenderEditorEntities(World& world, Entity* camera, bool soun
         // Render physics.
         if (physics) {
             for (Component::Physics* physics : Managers().physicsManager->GetPhysicsComponents()) {
-                const Physics::Shape& shape = physics->GetShape();
-                if (shape.GetKind() == Physics::Shape::Kind::Sphere) {
+                const ::Physics::Shape& shape = physics->GetShape();
+                if (shape.GetKind() == ::Physics::Shape::Kind::Sphere) {
                     Managers().debugDrawingManager->AddSphere(physics->entity->position, shape.GetSphereData()->radius, glm::vec3(1.0f, 1.0f, 1.0f));
-                } else if (shape.GetKind() == Physics::Shape::Kind::Plane) {
+                } else if (shape.GetKind() == ::Physics::Shape::Kind::Plane) {
                     Managers().debugDrawingManager->AddPlane(physics->entity->position, shape.GetPlaneData()->normal, glm::vec2(1.0f, 1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
                 }
             }
