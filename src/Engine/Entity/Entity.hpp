81cf5f2781efcb7e20dd15d89f7dcc74b386f981
Lukas Ljungberg <lukas-ljungberg@outlook.com>
2017-10-02 11:35:55 +0200

    removed parameter comment isSameScene

--- src/Engine/Entity/Entity.hpp
+++ src/Engine/Entity/Entity.hpp
@@ -60,7 +60,6 @@ class Entity {
         /// Instantiate a scene as a child to this entity.
         /**
          * @param name The name of the scene to instantiate.
-         * @param isSameScene set to true if it is the same scene you want to istantiate.
          * @return The created root entity of the scene.
          */
         Entity* InstantiateScene(const std::string& name, const std::string& originScene);
@@ -69,6 +68,7 @@ class Entity {
         /**
         * @param filename The name of the scene to check.
         * @param error Set to true inside the function if it allready exists.
+        * @param originScene Name of scene you want to check.
         * @param root The json value of root scene.
         */
         void CheckIfSceneExists(std::string filename, bool & error, std::string originScene, Json::Value root);
