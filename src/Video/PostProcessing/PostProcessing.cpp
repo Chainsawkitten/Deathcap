e0d43e7144594ff2140f07174c6ae87a8338d301
DESKTOP-D63EFMG\ivar <ivarbjorling@hotmail.com>
2017-10-10 15:51:59 +0200

    Removed unused filters

--- src/Video/PostProcessing/PostProcessing.cpp
+++ src/Video/PostProcessing/PostProcessing.cpp
@@ -4,9 +4,7 @@
 #include <Video/Geometry/Rectangle.hpp>
 #include <Video/PostProcessing/Filter.hpp>
 #include "Shader/Shader.hpp"
-#include <PostCopy.frag.hpp>
 #include <Post.vert.hpp>
-#include <PostDither.frag.hpp>
 #include "RenderSurface.hpp"
 #include "Buffer/ReadWriteTexture.hpp"
 #include "Buffer/FrameBuffer.hpp"
@@ -14,24 +12,10 @@
 using namespace Video;
 
 PostProcessing::PostProcessing(const Geometry::Rectangle* rectangle) {
-    
     this->rectangle = rectangle;
-
-    Shader* vertexShader = new Shader(POST_VERT, POST_VERT_LENGTH, GL_VERTEX_SHADER);
-    Shader* fragmentShader = new Shader(POSTCOPY_FRAG, POSTCOPY_FRAG_LENGTH, GL_FRAGMENT_SHADER);
-    shaderProgram = new ShaderProgram({ vertexShader, fragmentShader });
-    delete fragmentShader;
-
-    Shader* ditherFragmentShader = new Shader(POSTDITHER_FRAG, POSTDITHER_FRAG_LENGTH, GL_FRAGMENT_SHADER);
-    ditherShaderProgram = new ShaderProgram({ vertexShader, ditherFragmentShader });
-
-    delete vertexShader;
-    delete ditherFragmentShader;
 }
 
 PostProcessing::~PostProcessing() {
-    delete shaderProgram;
-    delete ditherShaderProgram;
 }
 
 void PostProcessing::ApplyFilter(Video::RenderSurface* renderSurface, Video::Filter* filter) const {
@@ -57,38 +41,6 @@ void PostProcessing::ApplyFilter(Video::RenderSurface* renderSurface, Video::Fil
     glDrawElements(GL_TRIANGLES, rectangle->GetIndexCount(), GL_UNSIGNED_INT, (void*)0);
     
     renderSurface->GetColorFrameBuffer()->Unbind();
-
-    // Reset depth testing to standard value.
-    glDepthFunc(GL_LESS);
-}
-
-void PostProcessing::Render(RenderSurface* renderSurface, bool dither) {
-    // Render to backbuffer.
-    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-
-    // Always pass depth test.
-    glDepthFunc(GL_ALWAYS);
-
-    ShaderProgram* shader = dither ? ditherShaderProgram : shaderProgram;
-    shader->Use();
-
-    // Swap render surface read/write buffers.
-    renderSurface->Swap();
-
-    glUniform1i(shader->GetUniformLocation("tDiffuse"), 0);
-    renderSurface->GetColorTexture()->BindForReading(GL_TEXTURE0);
-
-    glUniform1i(shader->GetUniformLocation("tDepth"), 1);
-    renderSurface->GetDepthTexture()->BindForReading(GL_TEXTURE1);
-
-    if (dither) {
-        glUniform1f(shader->GetUniformLocation("time"), ditherTime);
-        ditherTime = fmod(ditherTime + 1.f, 255.f);
-    }
-
-    glBindVertexArray(rectangle->GetVertexArray());
-
-    glDrawElements(GL_TRIANGLES, rectangle->GetIndexCount(), GL_UNSIGNED_INT, (void*)0);
 
     // Reset depth testing to standard value.
     glDepthFunc(GL_LESS);
