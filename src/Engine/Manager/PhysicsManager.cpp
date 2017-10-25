Chainsawkitten/LargeGameProjectEngine

Methods to listen to trigger events on PhysicsManager now properly takes a trigger volume instead of a rigid body. Trigger related code in scripts has been removed for compilation purposes and in preparation of trigger system.

src/Engine/Manager/PhysicsManager.cpp
@@ -76,30 +76,27 @@ void PhysicsManager::UpdateEntityTransforms() {
     }
 }
 
-void PhysicsManager::OnTriggerEnter(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerEnter(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](Physics::TriggerObserver& observer) {
             observer.OnEnter(callback);
         });
     });
 }
 
-void PhysicsManager::OnTriggerRetain(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerRetain(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](::Physics::TriggerObserver& observer) {
             observer.OnRetain(callback);
         });
     });
 }
 
-void PhysicsManager::OnTriggerLeave(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerLeave(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](::Physics::TriggerObserver& observer) {
             observer.OnLeave(callback);
         });

src/Engine/Manager/PhysicsManager.hpp
@@ -51,7 +51,7 @@ class PhysicsManager {
          * @param object Body that is to enter the trigger volume.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerEnter(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerEnter(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Set up listener for when |object| is intersecting |trigger|.
         /**
@@ -59,7 +59,7 @@ class PhysicsManager {
          * @param object Body that is to cause trigger to fire.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerRetain(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerRetain(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Set up listener for when |object| has left |trigger|.
         /**
@@ -67,7 +67,7 @@ class PhysicsManager {
          * @param object Body that is to cause trigger to fire.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerLeave(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerLeave(Util::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Create rigid body component.
         /**

Chainsawkitten/Deathcap

src/Engine/Manager/PhysicsManager.cpp
@@ -76,30 +76,27 @@ void PhysicsManager::UpdateEntityTransforms() {
     }
 }
 
-void PhysicsManager::OnTriggerEnter(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerEnter(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](Physics::TriggerObserver& observer) {
             observer.OnEnter(callback);
         });
     });
 }
 
-void PhysicsManager::OnTriggerRetain(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerRetain(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](::Physics::TriggerObserver& observer) {
             observer.OnRetain(callback);
         });
     });
 }
 
-void PhysicsManager::OnTriggerLeave(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback) {
-    auto t = CreateTrigger(trigger);
+void PhysicsManager::OnTriggerLeave(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback) {
     // Add the callback to the trigger observer
-    t.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
+    trigger.Open(triggerLockBoxKey, [object, &callback](Physics::Trigger& trigger) {
         trigger.ForObserver(object->GetBulletRigidBody(), [&callback](::Physics::TriggerObserver& observer) {
             observer.OnLeave(callback);
         });

src/Engine/Manager/PhysicsManager.hpp
@@ -51,7 +51,7 @@ class PhysicsManager {
          * @param object Body that is to enter the trigger volume.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerEnter(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerEnter(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Set up listener for when |object| is intersecting |trigger|.
         /**
@@ -59,7 +59,7 @@ class PhysicsManager {
          * @param object Body that is to cause trigger to fire.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerRetain(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerRetain(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Set up listener for when |object| has left |trigger|.
         /**
@@ -67,7 +67,7 @@ class PhysicsManager {
          * @param object Body that is to cause trigger to fire.
          * @param callback Function to call when resolving event.
          */
-        ENGINE_API void OnTriggerLeave(Component::RigidBody* trigger, Component::RigidBody* object, std::function<void()> callback);
+        ENGINE_API void OnTriggerLeave(Utility::LockBox<Physics::Trigger> trigger, Component::RigidBody* object, std::function<void()> callback);
 
         /// Create rigid body component.
         /**