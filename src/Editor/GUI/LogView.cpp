4b7e0eda45cf6ef8d0afc13a2ef02bd7d9cbe546
danzer <danielpejcinovski@hotmail.com>
2017-10-27 14:26:11 +0200

    Stashing changes.

--- src/Editor/GUI/LogView.cpp
+++ src/Editor/GUI/LogView.cpp
@@ -1,10 +1,38 @@
 #include "LogView.hpp"
 #include <imgui.h>
+#include <array>
 #include <Engine/MainWindow.hpp>
 #include "../ImGui/Splitter.hpp"
-#include "Editor.hpp"
+#include <Utility/Log.hpp>
+#include <iostream>
 
 using namespace GUI;
+
+
+LogView::LogView() {
+    defaultBuffer = new std::basic_stringbuf<char>();
+    infoBuffer = new std::basic_stringbuf<char>();
+    warningBuffer = new std::basic_stringbuf<char>();
+    errorBuffer = new std::basic_stringbuf<char>();
+
+    defaultStream = new LogStream(defaultBuffer);
+    infoStream = new LogStream(infoBuffer);
+    warningStream = new LogStream(warningBuffer);
+    errorStream = new LogStream(errorBuffer);
+    Log().SetupStreams(defaultStream, infoStream, warningStream, errorStream, &std::cout);
+}
+
+LogView::~LogView() {
+    delete defaultBuffer;
+    delete infoBuffer;
+    delete warningBuffer;
+    delete errorBuffer;
+
+    delete defaultStream;
+    delete infoStream;
+    delete warningStream;
+    delete errorStream;
+}
 
 void LogView::Show() {
     ImVec2 size(MainWindow::GetInstance()->GetSize().x, MainWindow::GetInstance()->GetSize().y);
@@ -17,11 +45,21 @@ void LogView::Show() {
     ImGui::SetNextWindowPos(ImVec2(sceneWidth, 20));
     ImGui::SetNextWindowSize(ImVec2(size.x - sceneWidth - editorWidth, logHeight));
 
-    ImGui::Begin("Log", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_ShowBorders);
+    std::string textMessage = stringBuffer->str() + "\n";
 
+    textBuffer.appendv(textMessage.c_str(), nullptr);
+
+    ImGui::Begin("Log", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_ShowBorders);
+    ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+    ImGui::TextUnformatted(textBuffer.begin());
+    ImGui::EndChild();
     ImGui::End();
 }
 
 bool LogView::IsVisible() {
     return visible;
+}
+
+LogView::LogStream::LogStream(std::stringbuf* stringBuffer) : std::ostream(stringBuffer) {
+
 }
--- src/Editor/GUI/LogView.hpp
+++ src/Editor/GUI/LogView.hpp
@@ -1,13 +1,37 @@
 #pragma once
+#include <sstream>
+#include <imgui.h>
 
 namespace GUI {
     class LogView {
         public:
+        LogView();
+
+        ~LogView();
+
         void Show();
 
         bool IsVisible();
         
         private:
+        class LogStream : public std::ostream {
+        public:
+            LogStream(std::stringbuf* stringBuffer);
+        };
+
+        LogStream* defaultStream;
+        LogStream* infoStream;
+        LogStream* warningStream;
+        LogStream* errorStream;
+
+        std::stringbuf* defaultBuffer;
+        std::stringbuf* infoBuffer;
+        std::stringbuf* warningBuffer;
+        std::stringbuf* errorBuffer;
+
+        ImGuiTextBuffer textBuffer;
+        ImGuiTextFilter textFilter;
+
         bool visible = true;
 
         static const int splitterSize = 2;
