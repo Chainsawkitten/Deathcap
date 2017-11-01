4c81f391c3b18ba0f5a8abb8a1b4765aa0d9cba6
NinjaDanz3r <NinjaDanz3r@users.noreply.github.com>
2017-11-01 09:18:40 +0100

    Update LogView.hpp
    Indented header.

--- src/Editor/GUI/LogView.hpp
+++ src/Editor/GUI/LogView.hpp
@@ -5,41 +5,41 @@
 namespace GUI {
     class LogView {
         public:
-        /// Constructor.
-        LogView();
+            /// Constructor.
+            LogView();
 
-        /// Destructor.
-        ~LogView();
+            /// Destructor.
+            ~LogView();
 
-        /// Shows the log.
-        void Show();
+            /// Shows the log.
+            void Show();
 
-        /// If the log is visible or not.
-        /**
-         * @return Whether the log is visible or not.
-         */
-        bool IsVisible();
+            /// If the log is visible or not.
+            /**
+             * @return Whether the log is visible or not.
+             */
+            bool IsVisible();
         
         private:
-        std::stringstream defaultStringstream;
-        std::stringstream infoStringstream;
-        std::stringstream warningStringstream;
-        std::stringstream errorStringstream;
+            std::stringstream defaultStringstream;
+            std::stringstream infoStringstream;
+            std::stringstream warningStringstream;
+            std::stringstream errorStringstream;
 
-        ImGuiTextBuffer textBuffer;
-        ImGuiTextFilter textFilter;
-        ImVector<int> lineOffsets;
+            ImGuiTextBuffer textBuffer;
+            ImGuiTextFilter textFilter;
+            ImVector<int> lineOffsets;
 
-        bool visible = true;
+            bool visible = true;
 
-        static const int splitterSize = 2;
-        int logHeight = 250;
-        bool logResize = false;
+            static const int splitterSize = 2;
+            int logHeight = 250;
+            bool logResize = false;
 
-        int sceneWidth = 250;
-        bool sceneResize = false;
+            int sceneWidth = 250;
+            bool sceneResize = false;
 
-        int editorWidth = 250;
-        bool editorResize = false;
+            int editorWidth = 250;
+            bool editorResize = false;
     };
 }
