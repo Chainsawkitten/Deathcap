5d6db2f9c9a8992787d7c7ebdb89caf08a2d9e68
Andreas Flöjt <andenzz@hotmail.com>
2017-11-02 09:18:40 +0100

    Add conversion function from GLM quaternion to Bullet quaternion.

--- src/Engine/Physics/GlmConversion.cpp
+++ src/Engine/Physics/GlmConversion.cpp
@@ -10,6 +10,10 @@ namespace Physics {
         return glm::vec3(vec.getX(), vec.getY(), vec.getZ());
     }
 
+    btQuaternion glmToBt(const glm::quat& quat) {
+        return btQuaternion(quat.x, quat.y, quat.z, quat.w);
+    }
+
     glm::quat btToGlm(btQuaternion const& quat) {
         return glm::quat(quat.getW(), quat.getX(), quat.getY(), quat.getZ());
     }
