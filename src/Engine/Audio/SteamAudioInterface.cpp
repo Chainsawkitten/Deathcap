afb2450ff1e7e4fe376c3d05aa2400a85a4958e7
NiceGuyAndersson <oscar@niceguyandersson.com>
2017-11-02 15:13:31 +0100

    Some initial function definitions

--- src/Engine/Audio/SteamAudioInterface.cpp
+++ src/Engine/Audio/SteamAudioInterface.cpp
@@ -2,18 +2,77 @@
 
 
 SteamAudioInterface::SteamAudioInterface() {
-    context = nullptr;
     playerDir = new IPLVector3{ 0, 0, 0 };
     playerPos = new IPLVector3{ 0, 0, 0 };
     playerUp = new IPLVector3{ 0, 0, 0 };
+
+    simSettings.sceneType = IPL_SCENETYPE_PHONON;
+    simSettings.numRays = 64000;
+    simSettings.numDiffuseSamples = 32;
+    simSettings.numBounces = 2;
+    simSettings.irDuration = 1.0;
+    simSettings.ambisonicsOrder = 0;
+    simSettings.maxConvolutionSources = 0;
 }
 
 SteamAudioInterface::~SteamAudioInterface() {
-    delete context;
     // Might have to remove these depending on how the pointers are sent to this class
     delete playerDir;
     delete playerPos;
     delete playerUp;
+}
+
+void SteamAudioInterface::SetContext(IPLLogFunction logCallback, IPLAllocateFunction allocCallback, IPLFreeFunction freeCallback) {
+
+}
+
+void SteamAudioInterface::CreateScene(uint32_t numMaterials) {
+    iplCreateScene(context, NULL, simSettings, numMaterials, scene);
+}
+
+void SteamAudioInterface::FinalizeScene(IPLFinalizeSceneProgressCallback progressCallback) {
+    iplFinalizeScene(scene, NULL);
+}
+
+SteamAudioInterface::SaveData SteamAudioInterface::SaveFinalizedScene() {
+    SaveData saveData;
+
+    // Call once first only to figure out the size of the saved scene.
+    saveData.sceneSize = iplSaveFinalizedScene(scene, NULL);
+
+    // Actually save the scene.
+    saveData.scene = new IPLbyte[saveData.sceneSize];
+    iplSaveFinalizedScene(scene, saveData.scene);
+
+    return saveData;
+}
+
+void SteamAudioInterface::LoadFinalizedScene(SaveData data) {
+    iplLoadFinalizedScene(context, simSettings, data.scene, data.sceneSize, NULL, NULL, scene);
+}
+
+void SteamAudioInterface::SetSceneMaterial(uint32_t matIndex, IPLMaterial material) {
+    iplSetSceneMaterial(scene, matIndex, material);
+}
+
+IPLhandle * SteamAudioInterface::CreateStaticMesh(std::vector<IPLVector3> vertices, std::vector<IPLTriangle> indices, int materialIndex) {
+    IPLhandle* mesh;
+
+    // Create mesh
+    iplCreateStaticMesh(scene, vertices.size(), indices.size() / 3, mesh);
+    iplSetStaticMeshVertices(scene, mesh, vertices.data());
+    iplSetStaticMeshTriangles(scene, mesh, indices.data());
+    iplSetStaticMeshMaterials(scene, mesh, &materialIndex);
+
+    return nullptr;
+}
+
+void SteamAudioInterface::CreateEnvironment(IPLSimulationSettings settings) {
+    iplCreateEnvironment(context, NULL, simSettings, scene, NULL, environment);
+}
+
+void SteamAudioInterface::SetPlayer(IPLVector3 * playerPos, IPLVector3 * playerDir, IPLVector3 * playerUp) {
+
 }
 
 void SteamAudioInterface::Process(float* input, uint32_t samples,  IPLVector3 * sourcePosition, float sourceRadius) {
--- src/Engine/Audio/SteamAudioInterface.hpp
+++ src/Engine/Audio/SteamAudioInterface.hpp
@@ -24,72 +24,59 @@ class SteamAudioInterface {
          */
         void SetContext(IPLLogFunction logCallback, IPLAllocateFunction allocCallback, IPLFreeFunction freeCallback);
     
-        /// Creates a scene object, to be populated with meshes.
+        /// Creates the scene object, to be populated with meshes.
         /**
          * @param settings The settings to use for simulation.
          * @param numMaterials Number of different materials used. May not be added or removed after Scene is created.
-         * @return Scene object to be used for adding meshes.
          */
-        IPLhandle* CreateScene(IPLSimulationSettings settings, uint32_t numMaterials);
+        void CreateScene(uint32_t numMaterials);
 
-        /// Destroys a scene object (once reference count reaches zero).
+        /// Finalizes the scene object.
         /**
-         * @param scene Handle to a scene to be destroyed.
-         */
-        void DestroyScene(IPLhandle* scene);
-
-        /// Finalizes a scene object.
-        /**
-         * @param scene Handle to the scene to be finalized.
          * @param progressCallback Callback to check the progress of the finalization. Can be NULL.
          */
-        void FinalizeScene(IPLhandle* scene, IPLFinalizeSceneProgressCallback progressCallback); //Can also create Environment (until we start using multiple scenes)
+        void FinalizeScene(IPLFinalizeSceneProgressCallback progressCallback); //Can also create Environment (until we start using multiple scenes)
     
         /// Everything needed to load a previously finalized scene stored in a byte array.
         struct SaveData {
             IPLSimulationSettings settings;
             int sceneSize;
-            char* scene;
+            IPLbyte* scene;
         };
 
-        /// Saves a finalized scene to a struct.
+        /// Saves the finalized scene to a struct.
         /**
-         * @param scene The scene to save
          * @return A struct containing everything needed to reconstruct the scene with LoadFinalizedScene().
          */
-        SaveData SaveFinalizedScene(IPLhandle* scene); //Note to self: Should call iplSaveFinalizedScene with null data pointer first to figure out how big to make the data pointer for a second call
+        SaveData SaveFinalizedScene(); //Note to self: Should call iplSaveFinalizedScene with null data pointer first to figure out how big to make the data pointer for a second call
 
         /// Loads a finalized scene from a struct.
         /**
          * @param data Struct containing all data relevant to reconstructing a scene
-         * @return Scene
          */
-        IPLhandle* LoadFinalizedScene(SaveData data);
+        void LoadFinalizedScene(SaveData data);
 
-        /// Specifies a single material used by a scene
+        /// Specifies a single material used by the scene
         /**
          * @param matIndex Index of the material to set. Between 0 and N-1 where N is the value of numMaterials passed to CreateScene().
          * @param material The material properties to set.
-         * @param scene The scene to set the material for. If null, will use most recently created scene.
          **/
-        void SetSceneMaterial(uint32_t matIndex, IPLMaterial material, IPLhandle* scene = nullptr);
+        void SetSceneMaterial(uint32_t matIndex, IPLMaterial material);
 
-        /// Creates a static mesh and adds it to a non-finalized scene.
+        /// Creates a static mesh and adds it to the non-finalized scene.
         /**
-         * @param scene Scene to add mesh to.
          * @param verices Vector of all vertices of the mesh.
          * @param indices Vector of all indices of the mesh.
          * @param materialIndex Index of the material to be used. Previously specified in SetSceneMaterial.
          * @return A handle to the static mesh (automatically added to the scene).
          */
-        IPLhandle* CreateStaticMesh(IPLhandle* scene, std::vector<IPLVector3> vertices, std::vector<IPLVector3> indices, uint32_t materialIndex);
+        IPLhandle* CreateStaticMesh(std::vector<IPLVector3> vertices, std::vector<IPLTriangle> indices, int materialIndex);
 
         /// Creates an Environment object for use by the audio engine internally.
         /**
-         * @param scene Scene to create Environment from, can be NULL to use direct sound only.
          * @param settings Settings to use for simulation. Must be the same settings used when created the scene!
          */
-        void CreateEnvironment(IPLhandle* scene, IPLSimulationSettings settings); //Should save the environment in the class
+        void CreateEnvironment(IPLSimulationSettings settings); //Should save the environment in the class
 
         /// Give pointers to player positional data.
         /**
@@ -116,13 +103,15 @@ class SteamAudioInterface {
 
     private:
 
-        IPLContext* context;
+        IPLContext context;
 
         IPLRenderingSettings renderSettings;
+        IPLSimulationSettings simSettings;
 
         SteamAudio sAudio;
 
-        std::vector<IPLhandle*> scenes;
+        IPLhandle* scene;
+        IPLhandle* environment;
 
         IPLVector3* playerPos;
         IPLVector3* playerDir;
