cc75ff6e08aa74465fe7b9223d7f2c54cf9569d6
Andreas Flöjt <andenzz@hotmail.com>
2017-11-17 15:11:04 +0100

    Fix missing whitespace after comma.

--- src/Editor/GUI/Editors/CurveEditor.cpp
+++ src/Editor/GUI/Editors/CurveEditor.cpp
@@ -341,9 +341,9 @@ namespace ImGui {
     void spline(const float *key, int num, int dim, float t, float *v) {
         static signed char coefs[16] = {
             -1, 2, -1, 0,
-            3,-5, 0, 2,
+            3, -5, 0, 2,
             -3, 4, 1, 0,
-            1,-1, 0, 0 };
+            1, -1, 0, 0 };
 
         const int size = dim + 1;
 
