5f3af30faa442188ff3bb32f825dcd5fd457bd05
Itamar Kupersmidt <itamar.kup@gmail.com>
2017-11-22 15:16:39 +0100

    Fixed memset

--- src/Engine/Audio/SoundBuffer.cpp
+++ src/Engine/Audio/SoundBuffer.cpp
@@ -37,7 +37,7 @@ float* SoundBuffer::GetChunkData(int& samples) {
     while (!handle.done);
 
     if (handle.samples < CHUNK_SIZE)
-        std::memset(&handle.data[(handle.offset + handle.samples) % (CHUNK_SIZE * CHUNK_COUNT)], 0, sizeof(CHUNK_SIZE - handle.samples));
+        memset(&handle.data[(handle.offset + handle.samples) % (CHUNK_SIZE * CHUNK_COUNT)], 0, sizeof(CHUNK_SIZE - handle.samples));
 
     samples = handle.samples;
     return handle.data;
