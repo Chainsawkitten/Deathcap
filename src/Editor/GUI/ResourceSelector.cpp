b15aa54f0532103c3d690249a57a60f93493746e
Andreas Flöjt <andenzz@hotmail.com>
2017-12-06 13:06:51 +0100

    Remove some whitespace.

--- src/Editor/GUI/ResourceSelector.cpp
+++ src/Editor/GUI/ResourceSelector.cpp
@@ -20,7 +20,6 @@ const ResourceSelector::SelectedResource& ResourceSelector::GetSelectedResource(
 
 bool ResourceSelector::ShowHelper(ResourceList::Resource::Type type, const ResourceList::ResourceFolder& folder, const std::string& path) {
     // Show subfolders.
-
     for (const ResourceList::ResourceFolder& subfolder : folder.subfolders) {
         if (ImGui::TreeNode(subfolder.name.c_str())) {
             bool subfolderSelected = ShowHelper(type, subfolder, path + "/" + subfolder.name);
@@ -29,7 +28,7 @@ bool ResourceSelector::ShowHelper(ResourceList::Resource::Type type, const Resou
                 return true;
         }
     }
-    
+
     // Show resources.
     for (const ResourceList::Resource& resource : folder.resources) {
         if (resource.type == type) {
@@ -48,6 +47,6 @@ bool ResourceSelector::ShowHelper(ResourceList::Resource::Type type, const Resou
             }
         }
     }
-    
+
     return false;
 }
